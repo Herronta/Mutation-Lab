39711bcb1feb7099243fb3d6e4eacb92c6fd9886
Herronta <46303636+Herronta@users.noreply.github.com>
2020-04-16 16:24:23 -0400

    Main

---
+++ Mutation-Lab-code/src/test_Mutation_Lab_JUnitTest/Mutation_Lab_JUnitTest.java
@@ -0,0 +1,14 @@
+package test_Mutation_Lab_JUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Mutation_Lab_JUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
