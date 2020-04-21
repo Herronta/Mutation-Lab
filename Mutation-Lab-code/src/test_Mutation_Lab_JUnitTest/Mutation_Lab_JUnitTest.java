3904df18a23c83ed12de0cc4ede15eedc81b8799
Matt Vanderhorst <vandermo@mail.uc.edu>
2020-04-21 09:58:25 -0400

    Unknown bugs

--- Mutation-Lab-code/src/test_Mutation_Lab_JUnitTest/Mutation_Lab_JUnitTest.java
+++ Mutation-Lab-code/src/test_Mutation_Lab_JUnitTest/Mutation_Lab_JUnitTest.java
@@ -2,13 +2,52 @@ package test_Mutation_Lab_JUnitTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 import org.junit.jupiter.api.Test;
 
 class Mutation_Lab_JUnitTest {
 
+	//@Test
+	//void test() {
+	//	fail("Not yet implemented");
+	//}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	 void ReadTxtTest1() {
+		Map<Integer, String> InputPeopleInfo = new HashMap<Integer, String>();
+		int PersonUniqueID=0;
+		String path= "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String st;
+			List<String> tempList = new ArrayList<String>();
+			while ((st = br.readLine()) != null) {
+				tempList.add(st.trim().toString());
+			}
+			for (int i = 0; i < tempList.size(); i++) {
+				if (tempList.get(i).toString().contains("name:")) {
+					String tempAddress=tempList.get(i).trim()+","+tempList.get(i-1).trim();	//Changing tempList.get(i+1) to tempList.get(i-1)
+					assertEquals(tempList.get(i+1).trim(), tempList.get(i-1).trim());
+					InputPeopleInfo.put(PersonUniqueID, tempAddress);
+					PersonUniqueID++;				
+				}
+			}
+			br.close();
+	}
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Input file fail or cant read, check FileIO class");
+		}
 	}
 
 }
