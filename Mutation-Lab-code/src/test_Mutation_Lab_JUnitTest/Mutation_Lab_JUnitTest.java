8c523711f8efb9a0af65d92dab5cfbf8a9d5c683
E. Bach <bacheh@mail.uc.edu>
2020-04-23 16:24:48 -0400

    Tried to merge everything

--- Mutation-Lab-code/src/test_Mutation_Lab_JUnitTest/Mutation_Lab_JUnitTest.java
+++ Mutation-Lab-code/src/test_Mutation_Lab_JUnitTest/Mutation_Lab_JUnitTest.java
@@ -526,4 +526,59 @@ class Mutation_Lab_JUnitTest {
 			fail("Input file fail or can;t read, check FileIO class");
 		}
 	}
+	@Test
+	 void ReadTxtTest1() {
+		Map<Integer, String> InputPeopleInfo = new HashMap<Integer, String>();
+		int PersonUniqueID=0;
+		String path= "zipCode_info.xlsx";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String st;
+			List<String> tempList = new ArrayList<String>();
+			while ((st = br.readLine()) != null) {
+				tempList.add(st.trim().toString());
+			}
+			for (int i = 0; i < tempList.size(); i++) {
+				if (tempList.get(i).toString().contains("name:")) {
+					String tempAddress=tempList.get(i).trim()+","+tempList.get(i-1).trim();	//Changing tempList.get(i+1) to tempList.get(i-1)
+					assertEquals(tempList.get(i+1).trim(), tempList.get(i-1).trim());
+					InputPeopleInfo.put(PersonUniqueID, tempAddress);
+					PersonUniqueID++;				
+				}
+			}
+			br.close();
+	}
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Input file fail or cant read, check FileIO class");
+		}
+	}
+	
+	@Test
+	void ReadTxtTest2() {
+		Map<Integer, String> InputPeopleInfo = new HashMap<Integer, String>();
+		int PersonUniqueID=0;
+		String path= "zipCode_info.xlsx";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String st;
+			List<String> tempList = new ArrayList<String>();
+			while ((st = br.readLine()) != null) {
+				tempList.add(st.trim().toString());
+			}
+			for (int i = 0; i <= tempList.size(); i++) { //Changed (i < tempList.size() to i <= tempList.size())
+				assertEquals(tempList.size(), (tempList.size() + 1 )); // The (+ 1) is because it will have one more due to the <= 
+				if (tempList.get(i).toString().contains("name:")) {
+					String tempAddress=tempList.get(i).trim()+","+tempList.get(i+1).trim();	
+					InputPeopleInfo.put(PersonUniqueID, tempAddress);
+					PersonUniqueID++;				
+				}
+			}
+			br.close();
+	}
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Input file fail or cant read, check FileIO class");
+		}
+	}
 }
